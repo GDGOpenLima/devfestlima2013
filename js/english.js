7bbeaf4a43ce5f8430f3846811dd79a1f9436850
Armando <apiconz@gmail.com>
2017-08-18 12:15:04 -0500

    latest version

---
+++ js/english.js
@@ -0,0 +1,13 @@
+devfest.factory("Language", function(){
+    return {
+        "view_album" : "View Album",
+        "originally_shared" : "originally shared",
+        "shared" : "shared",
+        "toggle_navigation" : "Toggle navigation",
+        "home" : "Inicio",
+        "speakers": "Ponentes",
+        "schedule": "Agenda",
+        "news" : "Google+ Feed",
+        "sponsors" : "Sponsors y Comunidades"
+    }
+});
